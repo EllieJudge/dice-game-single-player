997118e250c1a73cb072717b20529ebacfd89797
EllieJudge <judge_ellie@hotmail.com>
2019-05-24 19:10:46 +0100

    initial commit

---
+++ dicegame.js
@@ -0,0 +1,44 @@
+let rollCount = 0;
+let randomNumber = 0;
+
+
+function rollDice() {  
+  randomNumber = (Math.floor(Math.random()*6)+1);
+  document.getElementById("numberz").innerHTML = ("Your score = " + Number.parseInt(rollCount += randomNumber));
+    
+  if (randomNumber == 1) {
+    //location.reload();
+    document.getElementById("score1").innerHTML = "You lose."
+    //location.reload();
+  }
+  else if (rollCount >= 20) {
+    document.getElementById("score2").innerHTML = "well done you win!!"
+  }
+}
+
+
+
+Button.onclick = function showNumberRolled () {
+ 
+  diceRoll = rollDice();
+  document.getElementById("dice").src = `dice${randomNumber}.png`
+  console.log('I have changed.')
+  
+}
+
+
+button.onclick = function restart () {
+// rollCount = 0;
+location.reload();
+}
+
+
+
+
+
+
+
+
+
+
+    
